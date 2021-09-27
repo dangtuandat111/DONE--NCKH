d853dc394510a3e103232a9d76597003a7018e71
dangtuandat111 <mrboss862000@gmail.com>
2021-09-27 10:31:13 +0700

    test loi

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -34,7 +34,7 @@
     <div class="card-body login-card-body">
       <p class="login-box-msg">Đăng nhập để bắt đầu</p>
 
-      <?php //Hiển thị thông báo thành công?>
+      
       @if ( Session::has('success') )
         <div class="alert alert-success alert-dismissible" role="alert">
           <strong>{{ Session::get('success') }}</strong>
@@ -44,17 +44,7 @@
           </button>
         </div>
       @endif
-      <?php //Hiển thị thông báo lỗi?>
-      @if ( Session::has('error') )
-        <div class="alert alert-danger alert-dismissible" role="alert">
-          <strong>{{ Session::get('error') }}</strong>
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            <span class="sr-only">Close</span>
-          </button>
-        </div>
-      @endif
-      @if ($errors->any())
+      @if(count($errors) > 0) 
         <div class="alert alert-danger alert-dismissible" role="alert">
           <ul>
                   @foreach ($errors->all() as $error)
